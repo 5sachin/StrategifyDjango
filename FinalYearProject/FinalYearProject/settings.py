526e4d79854990eed069d3180e9925bbb05c225a
5sachin <sachinmishra10072000@gmail.com>
2022-04-11 21:24:56 +0530

    update

--- FinalYearProject/FinalYearProject/settings.py
+++ FinalYearProject/FinalYearProject/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7z00sldl&vzf0a6tje@z3fve+7hs_qt846r%as0$a(8#mv4*t-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 LOGIN_REDIRECT_URL = '/'
 # Application definition
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
--- FinalYearProject/FinalYearProject/urls.py
+++ FinalYearProject/FinalYearProject/urls.py
@@ -15,8 +15,14 @@ Including another URLconincludef
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('Strategify.urls')),
     path('admin/', admin.site.urls),
+
+    path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
 ]
